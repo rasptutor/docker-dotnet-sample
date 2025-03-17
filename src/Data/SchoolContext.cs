00aaa573afa3ee5fb7daf496f427d1ad395523a2
nelio <njafonso@gmail.com>
2025-03-17 20:46:22 +0200

    c-sharpier

--- src/Data/SchoolContext.cs
+++ src/Data/SchoolContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+
 namespace myWebApp.Data
 {
-   public class SchoolContext : DbContext
-   {
-      public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
-      public DbSet<Models.Student>? Students { get; set; }
-   }
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options) { }
+
+        public DbSet<Models.Student>? Students { get; set; }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,30 +1,29 @@
 using Microsoft.EntityFrameworkCore;
-using myWebApp.Models;
 using myWebApp.Data;
+using myWebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<SchoolContext>(options =>
-   options.UseNpgsql(builder.Configuration.GetConnectionString("SchoolContext")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SchoolContext"))
+);
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
-   var services = scope.ServiceProvider;
-   try
-   {
-       var context = services.GetRequiredService<SchoolContext>();
-       var created = context.Database.EnsureCreated();
-
-   }
-   catch (Exception ex)
-   {
-       var logger = services.GetRequiredService<ILogger<Program>>();
-       logger.LogError(ex, "An error occurred creating the DB.");
-   }
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        var created = context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
 }
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
